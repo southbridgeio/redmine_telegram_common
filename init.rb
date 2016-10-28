a294f03ab86e940f2f2bd0e591afd572d557668e
Artur Trofimov <arturtr@gmail.com>
2016-10-28 17:12:27 +1000

    init

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_telegram_common do
+  name 'Redmine Telegram Common plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
