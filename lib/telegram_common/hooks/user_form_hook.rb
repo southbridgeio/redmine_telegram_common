d78aec263894b94047b44c21c2f89ad56031ed00
cross <cross@vgg.ru>
2017-01-07 00:54:29 +0300

    Refactoring.

--- lib/telegram_common/hooks/user_form_hook.rb
+++
@@ -1,8 +0,0 @@
-module RedmineTelegramCommon
-  module Hooks
-    class UserFormHook < Redmine::Hook::ViewListener
-      render_on :view_users_form, :partial => "telegram_common/users/telegram_account", :user => @user
-      render_on :view_my_account, :partial => "telegram_common/users/telegram_account", :user => @user
-    end
-  end
-end
