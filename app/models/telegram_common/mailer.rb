c5aec45b20223fd5a74ff79f17b080fe218f0ccb
constXife <constxife@yandex.ru>
2017-06-30 10:25:37 +0700

    Update calling

--- app/models/telegram_common/mailer.rb
+++ app/models/telegram_common/mailer.rb
@@ -23,9 +23,9 @@ class TelegramCommon::Mailer < ActionMailer::Base
     logger.debug "telegram_account: #{telegram_account.inspect}"
     logger.debug "plugin_name: #{plugin_name}"
 
-    mail = mail to: @user.mail,
+    mail = mail(to: @user.mail,
          subject: I18n.t('telegram_common.mailer.telegram_connect.subject'),
-         template_path: 'telegram_common/mailer'
+         template_path: 'telegram_common/mailer')
 
     logger.debug mail.body.encoded if Rails.env.development? && mail
   end
