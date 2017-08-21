94c167a52dc6dfbfa4def9557420eb32639cf095
constXife <constxife@yandex.ru>
2017-08-21 19:10:28 +0700

    Bugfix with namespace

--- lib/telegram_common/bot.rb
+++ lib/telegram_common/bot.rb
@@ -30,7 +30,7 @@ module TelegramCommon
     private
 
     def initialize_command(command)
-      command.is_a?(Telegram::Bot::Types::Message) ? command : Telegram::Bot::Types::Message.new(command)
+      command.is_a?(::Telegram::Bot::Types::Message) ? command : ::Telegram::Bot::Types::Message.new(command)
     end
 
     def execute_command
