12803cd304c5fb712f592f6b83e01e0c7cc4cf60
Vladislav Yashin <renesis007@gmail.com>
2018-02-14 11:40:28 +0300

    Add tdlib requirement to settings (#39)

--- lib/telegram_common.rb
+++ lib/telegram_common.rb
@@ -6,14 +6,4 @@ module TelegramCommon
   def self.set_locale
     I18n.locale = Setting['default_language']
   end
-
-  module_function
-
-  def redmine_host_valid?
-    Setting.host_name.split(':').first != 'localhost'
-  end
-
-  def rails_env_valid?
-    Rails.env.production?
-  end
 end
