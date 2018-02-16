c8f200aaea564d059a522f5a70da217434b2f977
Vladislav Yashin <v.yashin.work@gmail.com>
2018-02-16 15:51:34 +0300

    Add check for bot collisions

--- lib/telegram_common.rb
+++ lib/telegram_common.rb
@@ -6,4 +6,19 @@ module TelegramCommon
   def self.set_locale
     I18n.locale = Setting['default_language']
   end
+
+  def self.bot_collisions?
+    tokens = []
+    if defined?(Intouch)
+      tokens << Setting.plugin_redmine_intouch['telegram_bot_token']
+    end
+    if defined?(RedmineChatTelegram)
+      tokens << Setting.plugin_redmine_chat_telegram['bot_token']
+    end
+    if defined?(Redmine2FA)
+      tokens << Setting.plugin_redmine_2fa['bot_token']
+    end
+    tokens.compact!
+    tokens.uniq.size != tokens.size
+  end
 end
