b82a7017efa1d7eb044db168e0c8a9ca83f85c81
Vladislav Yashin <v.yashin.work@gmail.com>
2018-02-22 18:21:23 +0300

    Use worker to handle updates in webhook

--- app/controllers/telegram_webhook_controller.rb
+++ app/controllers/telegram_webhook_controller.rb
@@ -1,17 +1,6 @@
 class TelegramWebhookController < ApplicationController
-  TYPES = %w(inline_query
-                chosen_inline_result
-                callback_query
-                edited_message
-                message
-                channel_post
-                edited_channel_post)
-
   def update
-    update = Telegram::Bot::Types::Update.new(params)
-    message = TYPES.reduce { |m, t| m || update.public_send(t) }
-    TelegramCommon.update_manager.handle_message(message)
-
+    TelegramHandlerWorker.perform_async(params)
     head :ok
   end
 end
---
+++ app/workers/telegram_handler_worker.rb
@@ -0,0 +1,29 @@
+class TelegramHandlerWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: :telegram
+
+  TYPES = %w(inline_query
+              chosen_inline_result
+              callback_query
+              edited_message
+              message
+              channel_post
+              edited_channel_post)
+
+  def perform(params)
+    update = Telegram::Bot::Types::Update.new(params)
+    message = TYPES.reduce { |m, t| m || update.public_send(t) }
+    message = types.inject(nil) { |acc, elem| acc || update.send(elem) }
+
+    if message.present?
+      TelegramCommon.update_manager.handle_message(message)
+    else
+      logger.fatal "Can't find message: #{params.to_json}"
+    end
+  end
+
+  def logger
+    @logger ||= Logger.new(Rails.root.join('log/telegram_common',
+                                           'telegram-handler.log'))
+  end
+end
