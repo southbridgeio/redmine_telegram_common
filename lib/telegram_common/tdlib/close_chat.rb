4e8d1a8e3e2050385ffd421df8bc82c360c96215
Vladislav Yashin <v.yashin.work@gmail.com>
2018-03-27 10:28:54 +0300

    Fix CloseChat command

--- lib/telegram_common/tdlib/close_chat.rb
+++ lib/telegram_common/tdlib/close_chat.rb
@@ -3,14 +3,21 @@ module TelegramCommon::Tdlib
     def call(chat_id)
       @client.on_ready do |client|
         me = client.broadcast_and_receive('@type' => 'getMe')
+        bot_id = Setting.find_by(name: 'plugin_redmine_telegram_common').value['bot_id'].to_i
+
         chat = client.broadcast_and_receive('@type' => 'getChat', 'chat_id' => chat_id)
 
-        client.broadcast_and_receive('@type' => 'getBasicGroupFullInfo',
+        group_info = client.broadcast_and_receive('@type' => 'getBasicGroupFullInfo',
                                      'basic_group_id' => chat.dig('type', 'basic_group_id')
-        )['members'].map { |m| m['user_id'] }.each do |user_id|
-          delete_member(chat_id, user_id) unless user_id == me['id']
+        )
+        return if group_info['@type'] == 'error'
+
+        group_info['members'].map { |m| m['user_id'] }.each do |user_id|
+          delete_member(chat_id, user_id) unless user_id.in?([me['id'], bot_id)
         end
+
         delete_member(chat_id, me['id'])
+        delete_member(chat_id, bot_id)
       end
     end
 
