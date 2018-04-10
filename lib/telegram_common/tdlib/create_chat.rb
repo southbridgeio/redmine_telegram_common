0b786018ea405ca85395dd83dcf399b1ef1eb9e1
Vladislav Yashin <v.yashin.work@gmail.com>
2018-04-10 16:36:05 +0300

    Try to avoid rack-timeout

--- lib/telegram_common/tdlib/create_chat.rb
+++ lib/telegram_common/tdlib/create_chat.rb
@@ -1,14 +1,18 @@
 module TelegramCommon::Tdlib
   class CreateChat < Command
     def call(title, user_ids)
-      @client.on_ready do |client|
+      @client.on_ready(limit: 5) do |client|
         user_ids.each do |id|
           client.broadcast_and_receive('@type' => 'getUser', 'user_id' => id)
         end
 
+        sleep 1
+
         chat = client.broadcast_and_receive('@type' => 'createNewBasicGroupChat',
                                             'title' => title,
                                             'user_ids' => user_ids)
+
+        sleep 1
 
         client.broadcast_and_receive('@type' => 'toggleBasicGroupAdministrators',
                                      'basic_group_id' => chat.dig('type', 'basic_group_id'),
